17c3fad9c56c4643d40b6b4b6271fdad0482cc9c
MaximKononenko <kononenkomi@gmail.com>
2018-06-07 12:34:06 +0300

    docker

--- infr/deploy.sh
+++ infr/deploy.sh
@@ -10,6 +10,21 @@ tmpLog="tmp.log"
 mkdir eb && cd eb
 
 # Create Dockerrun file
+
+#FROM maven:3.3-jdk-8 as BUILD
+#WORKDIR /usr/src/
+#COPY . .
+#RUN cd initial && mvn package
+# STAGE 3 - Pack container
+#FROM openjdk:8
+#EXPOSE 8080
+#RUN mkdir app
+#WORKDIR /app
+#COPY --from=BUILD /usr/src/initial/target/gs-spring-boot-0.1.0.jar /app/
+#ENTRYPOINT ["java", "-jar", "gs-spring-boot-0.1.0.jar"]
+#docker build --tag my_local_maven:3.5.3-jdk-8 .
+#docker run -i --rm -p 80:8080 my_local_maven:3.5.3-jdk-8 -v "$(pwd)":/app -w /app java -jar gs-spring-boot-0.1.0.jar &
+
 cat >> Dockerrun.aws.json << EOF
 {
     "AWSEBDockerrunVersion": "1",
